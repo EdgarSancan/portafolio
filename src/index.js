bd3b1a5a00cf5366c990751f33637e6290d68986
Edgar <sancanperezedgar@gmail.com>
2023-05-17 18:40:42 -0500

    cambio

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const fn = () => {
+    const miString = "soy edgar"
+    let b = miString + "sancan"
+    console.log(b)
+}
+
+fn()
+const clickeado = () => {
+    let a = "ingeniero"
+    console.log(a)
+}
